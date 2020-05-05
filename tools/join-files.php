5f84d76f13a5258bedd85fa857a1c9cbc729d262
kujirahand <kujira@kujirahand.com>
2020-05-05 17:55:23 +0900

    add tools join-files.php / fixed: tab

---
+++ tools/join-files.php
@@ -0,0 +1,16 @@
+<?php
+include __DIR__.'/fileinfo.ini.php';
+
+$text = "";
+$files = glob($dir_src."/*.txt");
+sort($files);
+foreach ($files as $f) {
+  echo "$f\n";
+  $s = file_get_contents($f);
+  $s = str_replace("\r\n", "\n", $s);
+  $text .= trim($s). "\n";
+}
+file_put_contents($file_text_all, $text);
+echo "ok\n";
+
+

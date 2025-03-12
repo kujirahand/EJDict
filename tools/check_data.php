eab9b1790bbf462057819c86217801d580460ae7
kujirahand <web@kujirahand.com>
2025-03-12 21:34:58 +0900

    ファイルのエラーチェックを強化

---
+++ tools/check_data.php
@@ -0,0 +1,39 @@
+<?php
+$root = dirname(__DIR__);
+// check files
+$files = glob("$root/src/*.txt");
+for ($i = ord("a"); $i <= ord("z"); $i++) {
+    $a = chr($i);
+    $full = "$root/src/$a.txt";
+    if (file_exists($full)) {
+        continue;
+    }
+    echo "[ERROR] file not found: $full\n";
+    exit(-1);
+}
+// check lines
+$flagError = FALSE;
+foreach ($files as $file) {
+    $fname = basename($file);
+    echo "--- $fname ---\n";
+    $txt = file_get_contents($file);
+    $lines = explode("\n", $txt);
+    foreach ($lines as $line) {
+        $line = trim($line);
+        if ($line === "") {
+            continue;
+        }
+        $cells = explode("\t", $line);
+        if (count($cells) === 2) {
+            continue;
+        }
+        echo "[ERROR]($fname) $line\n";
+        $flagError = TRUE;
+    }
+}
+if ($flagError) {
+    echo "ng\n";
+    exit(1);
+}
+echo "ok\n";
+exit(0);
--- tools/makezip.php
+++ tools/makezip.php
@@ -1,10 +1,8 @@
 <?php
-include __DIR__."/fileinfo.ini.php";
+include __DIR__ . "/fileinfo.ini.php";
 
 $txt_zip = "ejdic-hand-txt.zip";
 $sql_zip = "ejdic-hand-sqlite.zip";
 chdir($dir_out);
 system("zip -r $txt_zip README.txt ejdict-hand-utf8.txt");
 system("zip -r $sql_zip README.txt ejdict.sqlite3");
-
-
